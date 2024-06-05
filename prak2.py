fbeed4411a6b4ff7ea662b4176752f0cc9177b2d
NelaNurazizah <158980145+NelaNurazizah@users.noreply.github.com>
2024-06-05 19:18:07 +0700

    Create prak2.py

---
+++ prak2.py
@@ -0,0 +1,43 @@
+print(“Praktikum Algoritma dan Pemrograman 1 - Variabel dan Tipe Data”)
+	
+	# Tipe data boolean
+boolean_var = False
+print(boolean_var)
+print(type(boolean_var))
+
+# Tipe data string
+string_var = "ini adalah tulisan berupa String"
+print(string_var)
+
+# Tipe data integer
+integer_var = 100
+print(integer_var)
+
+# Tipe data float
+float_var = 0.001
+print(float_var)
+
+# Konversi bilangan heksadesimal menjadi desimal
+hex_var = 0x01
+print("bilangan desimal dari 0x01 adalah", hex_var)
+
+# Tipe data complex
+complex_var = 10 + 2j
+print(complex_var)
+print(type(complex_var))
+
+# List
+list_var = [96, 97, 98, 99, 100]
+print(list_var)
+
+# List of strings
+list_of_strings = ["seratus", "dua ratus", "tiga ratus"]
+print(list_of_strings)
+
+# Dictionary
+dictionary_var = {'nama': 'Ani', 'umur': 19}
+print(dictionary_var)
+
+# String dengan tanda kutip tunggal
+single_quote_str = "This string contains a single quote (') character."
+print(single_quote_str)
